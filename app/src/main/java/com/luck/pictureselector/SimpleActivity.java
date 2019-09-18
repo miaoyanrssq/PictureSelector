b9cf1523b15d7aa8a6af2166a950a55348bc8854
高阳· 甄 <627312987@qq.com>
2019-09-18 10:39:19 +0800

    测试在线图片裁剪

--- app/src/main/java/com/luck/pictureselector/SimpleActivity.java
+++ app/src/main/java/com/luck/pictureselector/SimpleActivity.java
@@ -1,10 +1,24 @@
 package com.luck.pictureselector;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.tools.AttrsUtils;
+import com.luck.picture.lib.tools.PictureFileUtils;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn_activity, btn_fragment;
@@ -28,9 +42,50 @@ public class SimpleActivity extends AppCompatActivity implements View.OnClickLis
                 startActivity(intent);
                 break;
             case R.id.btn_fragment:
-                intent = new Intent(SimpleActivity.this, PhotoFragmentActivity.class);
-                startActivity(intent);
+//                intent = new Intent(SimpleActivity.this, PhotoFragmentActivity.class);
+//                startActivity(intent);
+                startCrop("https://csi.tmuyun.com/webpic/W0201909/W020190912/W020190912658291496894.png");
                 break;
+        }
+    }
+
+
+    protected void startCrop(String originalPath) {
+        UCrop.Options options = new UCrop.Options();
+//        int toolbarColor = AttrsUtils.getTypeValueColor(this, R.color._4786ff);
+//        int statusColor = AttrsUtils.getTypeValueColor(this,  R.color._4786ff);
+//        int titleColor = AttrsUtils.getTypeValueColor(this,  R.color._4786ff);
+//        options.setToolbarColor(toolbarColor);
+//        options.setStatusBarColor(statusColor);
+//        options.setToolbarWidgetColor(titleColor);
+//        options.setCircleDimmedLayer(config.circleDimmedLayer);
+//        options.setShowCropFrame(config.showCropFrame);
+//        options.setShowCropGrid(config.showCropGrid);
+//        options.setDragFrameEnabled(true);
+//        options.setScaleEnabled(true);
+//        options.setRotateEnabled(true);
+//        options.setCompressionQuality(config.cropCompressQuality);
+        options.setHideBottomControls(true);
+//        options.setFreeStyleCropEnabled(true);
+        boolean isHttp = PictureMimeType.isHttp(originalPath);
+        String imgType = PictureMimeType.getLastImgType(originalPath);
+        Uri uri = isHttp ? Uri.parse(originalPath) : Uri.fromFile(new File(originalPath));
+        UCrop.of(uri, Uri.fromFile(new File(PictureFileUtils.getDiskCacheDir(this),
+                System.currentTimeMillis() + imgType)))
+                .withAspectRatio(2, 1)
+                .withMaxResultSize(400, 200)
+                .withOptions(options)
+                .start(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 69 && data != null) {
+            Uri uri = UCrop.getOutput(data);
+            if(uri != null) {
+                String url = uri.getPath();
+            }
         }
     }
 }
